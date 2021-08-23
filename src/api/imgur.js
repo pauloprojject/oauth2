e33f1241589ca84fcadf19b91d13a88b5d8f093b
Paulo Victor Calixto Silva <pvictor@indracompany.com>
2021-08-23 08:42:48 -0300

    initial commit

---
+++ src/api/imgur.js
@@ -0,0 +1,22 @@
+import qs from 'qs';
+import axios from 'axios';
+
+const CLIENT_ID = '048b23a1830c7ec'
+const ROOT_URL = 'https://api.imgur.com'
+
+export default{
+    login(){
+        const querystring = {
+            client_id: CLIENT_ID,
+            response_type: 'token'
+        };
+        window.location = `${ROOT_URL}/oauth2/authorize?${qs.stringify(querystring)}`;
+    },
+    fetchImages(token){
+        return axios.get(`${ROOT_URL}/3/account/me/images`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+    }
+}
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="ui secondary pointing menu">
+        <router-link to="/" class="active item">
+            Image Storage
+        </router-link>
+
+        <div class="right menu">
+            <div v-if="isLoggedIn" class="horizontal">
+                <router-link to="/" class="item">Galleries</router-link>
+                <router-link to="/upload" href="" class="item">Upload</router-link>
+                <a href="" class="item" @click="logout">Logout</a>
+            </div>
+            <a href="#" class="ui item" @click="login" v-else>Login</a>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+
+export default {
+    name: 'AppHeader',
+    computed: mapGetters(['isLoggedIn']),    
+    methods: mapActions(['login', 'logout'])
+}
+</script>
+
+<style scoped>
+.horizontal{
+    display: flex;
+    flex-direction: row;
+}
+</style>
